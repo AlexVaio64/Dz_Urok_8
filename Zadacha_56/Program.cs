6a639613cc827a071d5e598caaba99953df00fdc
Michel Nekrasov <moscou@inbox.ru>
2022-11-23 18:52:54 +0300

    add Zadacha_56 correction

--- Zadacha_56/Program.cs
+++ Zadacha_56/Program.cs
@@ -8,3 +8,54 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки 
 // с наименьшей суммой элементов: 1 строка.
 
+
+Console.WriteLine("Введите количество строк массива: ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбиков массива: ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("Наименьшая сумма элементов массива = " + (minsum) + " и находится в строке # " + (indexLine));
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
